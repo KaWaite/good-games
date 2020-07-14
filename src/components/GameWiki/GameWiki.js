17176ca742087f52d92a7d8485106991db8debb5
KaWaite <kyle.waite@outlook.com>
2020-07-14 13:25:31 +0900

    fix route for gamewiki api call

--- src/components/GameWiki/GameWiki.js
+++ src/components/GameWiki/GameWiki.js
@@ -11,7 +11,7 @@ export default function GameWiki(props) {
   useEffect(() => {
     const fetchGame = async () => {
       const data = await axios.get(
-        `https://vast-peak-54513.herokuapp.com/${params.id}`
+        `https://vast-peak-54513.herokuapp.com/game/${params.id}`
       );
       setGameInfo(data.data);
     };
