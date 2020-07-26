a660e97e8a6df1fc96edd60a362e782c2fe20716
KaWaite <kyle.waite@outlook.com>
2020-07-26 19:01:07 +0900

    Removed unused useEffect

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Redirect } from "react-router-dom";
 import { Typography, Grid, Divider, Avatar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
